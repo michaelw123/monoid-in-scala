ca20978b34f43586b3e3e60e3b4249dafd710435
michaelw123 <xiaoqunw@hotmail.com>
2020-07-22 14:47:47 -0400

    created project

--- src/main/scala/monoids/monoid.scala
+++ src/main/scala/monoids/monoid.scala
@@ -1,5 +1,12 @@
 package monoids
 
-class monoid {
-
+object monoid extends App {
+  trait Monoid[A] {
+    def zero: A
+    def op(a1: A, a2: A): A
+  }
+  case class MonoidInstance[A](z:A, f: (A,A) => A) extends Monoid[A] {
+    override def zero = z
+    override def op(a:A, b:A) = f(a,b)
+  }
 }
