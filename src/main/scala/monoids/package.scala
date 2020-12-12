f493b681db82300652ae9e10c5524fb1d64d76be
michaelw123 <xiaoqunw@hotmail.com>
2020-12-12 18:17:48 -0500

    added foldMap, mapFold

--- src/main/scala/monoids/package.scala
+++ src/main/scala/monoids/package.scala
@@ -11,11 +11,12 @@ package object monoids {
 
   implicit val intAdditionMonoid = MonoidInstance[Int](0, _ + _)
   implicit val stringConcatMonoid =  MonoidInstance[String]("", _ + _)
-  implicit val orMonoid = MonoidInstance[Boolean](false, _ || _)
-  implicit val andMonoid = MonoidInstance[Boolean](true, _ && _)
+  implicit val booleanOrMonoid = MonoidInstance[Boolean](false, _ || _)
+  implicit val booleanAndMonoid = MonoidInstance[Boolean](true, _ && _)
   implicit def setMonoid[A] = MonoidInstance[Set[A]](Set.empty[A], _ ++ _)
   implicit def listMonoid[A] = MonoidInstance[List[A]](Nil, _ ++ _)
-  implicit def functionMonoid[A,B](B: Monoid[B]): Monoid[A => B] = new Monoid[A => B] {
+  implicit def mapMonoid[K, V] = MonoidInstance[Map[K,V]](Map.empty[K, V], _ ++ _)
+  implicit def functionCompositionMonoid[A,B](B: Monoid[B]): Monoid[A => B] = new Monoid[A => B] {
     def op(f: A => B, g: A => B) = a => B.op(f(a), g(a))
     val zero: A => B = a => B.zero
   }
@@ -34,20 +35,20 @@ package object monoids {
         }
   }
 
-  implicit def mapMonoid[K, V](implicit vm: Monoid[V]): Monoid[Map[K, V]] = {
-    new Monoid[Map[K, V]] {
-      override final def op(m1: Map[K, V], m2: Map[K, V]): Map[K, V] =
-        (m1.keySet | m2.keySet).foldLeft(this.zero) {
-          case (acc, key) =>
-            acc + (key -> vm.op(
-              m1.getOrElse(key, default = vm.zero),
-              m2.getOrElse(key, default = vm.zero)
-            ))
-        }
-
-      override final val zero: Map[K, V] = Map.empty
-    }
-  }
+//  implicit def mapMonoid[K, V](implicit vm: Monoid[V]): Monoid[Map[K, V]] = {
+//    new Monoid[Map[K, V]] {
+//      override final def op(m1: Map[K, V], m2: Map[K, V]): Map[K, V] =
+//        (m1.keySet | m2.keySet).foldLeft(this.zero) {
+//          case (acc, key) =>
+//            acc + (key -> vm.op(
+//              m1.getOrElse(key, default = vm.zero),
+//              m2.getOrElse(key, default = vm.zero)
+//            ))
+//        }
+//
+//      override final val zero: Map[K, V] = Map.empty
+//    }
+//  }
   //implicit def numericMaxMonoid[A <: Numeric]
 
   //      trait Monoid2[F[_], A] {
--- src/test/scala/monoids/foldMap.scala
+++ src/test/scala/monoids/foldMap.scala
@@ -1,18 +1,31 @@
 package monoids
 
 object foldMap extends App {
-  def foldMap[A, B](s:Seq[A], f: A => B)(implicit m:Monoid[B]):B = {
+  def fold[A](s:Seq[A]) (implicit m:Monoid[A]):A =
     s.length match {
       case 0 => m.zero
-      case 1 => f(s.head)
+      case 1 => s.head
       case _ => {
         val (l, r) = s.splitAt(s.length / 2)
-        m.op(foldMap(l, f), foldMap(r, f))
+        m.op(fold(l), fold(r))
       }
     }
+  def foldMap[A, B](s:Seq[A], f: A => B)(implicit m:Monoid[A]):B = {
+    f(fold(s)(m))
+  }
+  def mapFold[A,B](s:Seq[A], f: A => B)(implicit m:Monoid[B]):B = {
+    fold(s.map(f))(m)
   }
   def complicatedcomputation(x:Int) = x.toString
+
   val x = foldMap(List(1,2,3,4,5), complicatedcomputation)
 
   println(x)
+  def length(s:String) = s.length
+  val l = foldMap(List("a", "bb", "ccc"), length)
+
+  println(l)
+
+  val l1 = mapFold(List("a", "bb", "ccc"), length)
+  println(l1)
 }
